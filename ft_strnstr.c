97a53959d20466ea5ddaa42a92b366e049f5eef8
Daniel Stelmach <dstelmac@c1r14s6.42lisboa.com>
2023-04-26 19:09:46 +0100

    add

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,14 +6,31 @@
 /*   By: dstelmac <dstelmac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 20:04:59 by dstelmac          #+#    #+#             */
-/*   Updated: 2023/04/25 20:11:17 by dstelmac         ###   ########.fr       */
+/*   Updated: 2023/04/26 19:09:14 by dstelmac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-strnstr(const char	*big, const char *little, size_t len)
-
+char    *ft_strnstr(const char	*big, const char *lit, size_t len)
 {
-	
+	size_t  i;
+    size_t  j;
+    
+    if (!lit)
+        return(big);
+    i = 0;
+    
+    while(big[i] != '\0' && i < len)
+    {
+        j = 0;
+        while(j < len && big[i+j] == lit [j])
+        {
+            if(lit [j + 1] == '\0')
+                return(big[i]);
+            j++;
+        }
+        i++;
+    }
+    return (0);
 }

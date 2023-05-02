9377d0f990f94dd8df1945de2af22371887967e2
ds <dstelmac>
2023-05-02 17:52:52 +0100

    add strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: dstelmac <dstelmac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 01:59:29 by dstelmac          #+#    #+#             */
-/*   Updated: 2023/05/01 02:49:48 by dstelmac         ###   ########.fr       */
+/*   Updated: 2023/05/01 02:52:10 by dstelmac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,11 @@
 char	*ft_strjoin(const char *s1, const char *s2)
 {
 	char	*dest;
-	
-	
 
 	dest = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (!dest)
 		return (NULL);
 	ft_memcpy(dest, s1, ft_strlen(s1));
-	ft_memcpy(dest + ft_strlen(s1) , s2, ft_strlen(s2)+1);
-	
+	ft_memcpy(dest + ft_strlen(s1), s2, ft_strlen(s2) + 1);
 	return (dest);
 }
